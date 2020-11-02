c94d68a7c698dcf2d912dbec44e1714747221e0f
MadMonkey87 <myself@philippewechsler.ch>
2020-11-02 10:21:31 +0100

    adapted for new water leakage model ids

--- drivers/zipato-water-leakage/driver.js
+++ drivers/zipato-water-leakage/driver.js
@@ -10,7 +10,7 @@ class ZipatoWaterLeakageDriver extends Driver {
 	}
 	
 	onPairListDevices(data, callback) {
-		this.getSensorsByCondition(device => device.modelid === 'HM-HS1WL-M' || device.modelid === 'FB56-WTS04HM1.1' || device.modelid === 'SWHM-I1' || device.modelid==='WaterSensor-EM', callback)
+		this.getSensorsByCondition(device => device.modelid === 'HM-HS1WL-M' || device.modelid === 'FB56-WTS04HM1.1' || device.modelid === 'SWHM-I1' || device.modelid==='WaterSensor-EM' || device.modelid==='WATER_CTPG' || device.modelid==='TS0207', callback)
 	}
 	
 }
