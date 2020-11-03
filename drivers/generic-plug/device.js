4c218a2bc2e1876da3055e44ca520f8ae5e6f20e
MadMonkey87 <myself@philippewechsler.ch>
2020-11-03 08:58:44 +0100

    generic switch supports up to 8 buttons

--- drivers/generic-plug/device.js
+++ drivers/generic-plug/device.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const Light = require('../Light')
-const { FlowCardTriggerDevice } = require('homey')
+const { FlowCardTriggerDevice, Homey } = require('homey')
 
 class GenericPlug extends Light {
-	
+
 	onInit() {
 		super.onInit()
 
 		this.setTriggers()
-		
+
 		this.log(this.getName(), 'has been initiated')
 	}
 
@@ -25,9 +25,10 @@ class GenericPlug extends Light {
 	}
 
 	fireEvent(event, state) {
-		// ignore
+		this.log('generic-plug-unhandled-fire-event', event, state, Homey.ManagerSettings.get('manufacturername'))
+		Homey.app.uploadUsageData('generic-plug-unhandled-fire-event', { event: event, state: state, manufacturer: Homey.ManagerSettings.get('manufacturername') })
 	}
-	
+
 }
 
 module.exports = GenericPlug
