c67b97c1face90b9d0cecf9079c7665ff1542cbc
MadMonkey87 <myself@philippewechsler.ch>
2020-12-14 18:30:05 +0100

    support newer heiman devices

--- drivers/heiman-smoke-detector/driver.js
+++ drivers/heiman-smoke-detector/driver.js
@@ -10,7 +10,7 @@ class HeimanSmokeDetectorDriver extends Driver {
 	}
 
 	onPairListDevices(data, callback) {
-		this.getSensorsByCondition(device => device.modelid === 'SmokeSensor-N-3.0' || device.modelid === 'GAS_V15' || device.modelid === 'SMOK_YDLV10' || device.modelid === 'SMOK_V16' || device.modelid === 'SmokeSensor-EM' || device.modelid === 'SmokeSensor-N-3.0', callback)
+		this.getSensorsByCondition(device => device.modelid === 'SmokeSensor-N-3.0' || device.modelid === 'GAS_V15' || device.modelid === 'SMOK_YDLV10' || device.modelid === 'SMOK_V16' || device.modelid === 'SmokeSensor-EM' || device.modelid === 'SmokeSensor-N-3.0' || device.modelid === 'SmokeSensor-EF-3.0', callback)
 	}
 
 }
