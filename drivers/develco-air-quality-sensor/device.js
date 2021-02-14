bdf300652a1645b8e0f4a5191459fa75fbdfab06
MadMonkey87 <myself@philippewechsler.ch>
2021-02-14 22:01:24 +0100

    Support for Develco/Frient air quality sensor

--- drivers/develco-air-quality-sensor/device.js
+++ drivers/develco-air-quality-sensor/device.js
@@ -3,13 +3,25 @@
 const Sensor = require('../Sensor')
 
 class DevelcoAirQualitySensor extends Sensor {
-	
+
 	onInit() {
 		super.onInit()
-		
+
+		initializeTriggers()
+
 		this.log(this.getName(), 'has been initiated')
 	}
-	
+
+	setCapabilityValue(name, value) {
+		if (name === 'measure_voc' && value != this.getCapabilityValue(name)) {
+			this.vocChanged.trigger(this, { measure_voc: value });
+		}
+		super.setCapabilityValue(name, value)
+	}
+
+	initializeTriggers() {
+		this.vocChanged = new Homey.FlowCardTrigger('measure_voc_changed').register();
+	}
 }
 
 module.exports = DevelcoAirQualitySensor
